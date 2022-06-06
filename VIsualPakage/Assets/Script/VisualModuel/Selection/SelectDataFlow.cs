65b2afbe76f7249e1ed3454c3d52489bc18dca21
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-07 08:44:22 +0900

    선택지 구현 끝 비주얼노벨 대표적인 기능은 구현 종료 퀄리티업중

--- VIsualPakage/Assets/Script/VisualModuel/Selection/SelectDataFlow.cs
+++ VIsualPakage/Assets/Script/VisualModuel/Selection/SelectDataFlow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SelectDataFlow : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class SelectDataFlow : MonoBehaviour
     bool selectIn = false;
     bool instButton = true;
     public int result = 0;
+    [Header("페이드를 위한 오브젝트(검은색 스프라이트 하나 깔아주면 됩니다)")]
+    public GameObject FadeOb;
 
 
     private void Awake()
@@ -20,6 +23,7 @@ public class SelectDataFlow : MonoBehaviour
         //만약 instButton이 트루면
         if (instButton == true)
         {
+            StartCoroutine(Fade());
             //textData에 리스트 갯수만큼
             for (int i = 0; i < textData.dataSet.selectionList.Count; i++)
             {
@@ -37,10 +41,23 @@ public class SelectDataFlow : MonoBehaviour
         }
         if (textData.obEle.Set == true)
         {
+            StartCoroutine(FadeOut());
             selectIn = true;
             result = textData.obEle.resultNum;
         }
 
         return selectIn;
+    }
+
+    IEnumerator Fade()
+    {
+        FadeOb.GetComponent<SpriteRenderer>().DOFade(0.5f, 1);
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator FadeOut()
+    {
+        FadeOb.GetComponent<SpriteRenderer>().DOFade(0f, 0.5f);
+        yield return new WaitForSeconds(1f);
     }
 }
